80b13cdc84e2c43e7bf22dc5a19ddde6913ca1f8
Maxwell Rogers <100175240+wiresgithub@users.noreply.github.com>
2022-06-11 11:28:56 -0400

    beginning instructions for activity

--- public/index.js
+++ public/index.js
@@ -1,16 +1,16 @@
-function main(){
+function main() {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
+    fetch('192.168.1.4:9000')
     let countValue = 0;
-
-    function increment(){
+    console.log(countValue)
+    function increment() {
         countValue++;
         countContainer.textContent = countValue;
     }
 
-    function decrement(){
+    function decrement() {
         countValue--;
         countContainer.textContent = countValue;
     }
